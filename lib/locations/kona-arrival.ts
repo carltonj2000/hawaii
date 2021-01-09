3885cb1af229ca731352d3f551a305fb0f12668e
Carlton Joseph <carlton.joseph@gmail.com>
2021-01-09 09:11:32 -1000

    add image resize code

--- lib/locations/kona-arrival.ts
+++ lib/locations/kona-arrival.ts
@@ -2,7 +2,7 @@ export default {
   location: "konaarrival",
   name: "Kona Arrival",
   svg: "airplane",
-  activities: ["South West #0795 from SJC 8:35 AM"],
-  date: new Date(2020, 11, 14, 12, 10),
+  activities: ["South West #3120 from SJC 7:55 AM"],
+  date: new Date(2021, 1, 7, 12, 10),
   coordinates: [19.73671402885999, -156.0425169042607],
 };
--- scripts/imgResize.ts
+++ scripts/imgResize.ts
@@ -1,17 +1,14 @@
 import colors from "colors";
 import path from "path";
-import fs from "fs-extra";
 import { locationsInfo } from "../lib/locations";
 import { contentBase, container } from "./contentBase";
-import { execSync } from "child_process";
-import exifParser from "exif-parser";
+import sharp from "sharp";
+import fs from "fs-extra";
 
-const originalSize = [6000, 4000];
-const [oriX, oriY] = originalSize;
-const sizeY = [1080, 720, 360, 180];
-const sizes = sizeY.map((y) => [(oriX * y) / oriY, y]);
+const ySizes = [1080, 720, 360, 180];
 const szOutStr = ([x, y]) => `${x}x${y}`;
-const szDirName = (sz) => path.join("resized", `size_${szOutStr(sz)}`);
+const szDirName = (sz: [number, number]) =>
+  path.join("resized", `size_${szOutStr(sz)}`);
 
 (async () => {
   const prjDir = "hawaii";
@@ -19,15 +16,42 @@ const szDirName = (sz) => path.join("resized", `size_${szOutStr(sz)}`);
   console.log("resizing:", colors.cyan(contentRoot));
   for (let l of locationsInfo) {
     if (!l.images) continue;
+    if (l.imgsSkip) continue;
     for (let i of l.images) {
       const imgDir = path.join(l.imageInfo[i[0]][0], i[1]);
       const imgDirOriginals = path.join(l.imageInfo[i[0]][0], i[1]);
       const imgPath = path.join(contentRoot, imgDir);
       console.log(colors.green(imgPath));
-      const imgBuff = fs.readFileSync(imgPath);
-      const parser = exifParser.create(imgBuff);
-      const result = parser.parse();
-      console.log(result);
+      let image = sharp(imgPath);
+      const metadata = await image.metadata();
+      const { height, width } = metadata;
+      console.log("From:", width, height);
+      const imagesResized = [];
+      for (let y of ySizes) {
+        const hResize = y;
+        const wResize = Math.round((width * hResize) / height);
+        const fileOutName = path.join(
+          contentRoot,
+          l.imageInfo[i[0]][0],
+          szDirName([wResize, hResize]),
+          i[1]
+        );
+        if (fs.existsSync(fileOutName)) {
+          console.log("Skipping", fileOutName);
+          continue;
+        }
+        console.log("Sizing To:", wResize, hResize);
+        const resizedImage = await image.resize(wResize).jpeg().toBuffer();
+        imagesResized.push({ filename: fileOutName, data: resizedImage });
+        image = sharp(resizedImage);
+      }
+      for (let imageResized of imagesResized) {
+        const { filename, data } = imageResized;
+        const dir = path.parse(filename).dir;
+        if (!fs.pathExistsSync(dir)) fs.mkdirSync(dir, { recursive: true });
+        await sharp(data).toFile(filename);
+        console.log("Wrote:", filename);
+      }
     }
   }
 })().catch((e) => console.log(e));
