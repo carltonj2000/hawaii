c25bbe675edbbfcf0a1ce24381b897d2720e542b
Carlton Joseph <carlton.joseph@gmail.com>
2021-01-12 10:26:49 -1000

    add volcano pics

--- lib/locations/kilaueaVolcano1.ts
+++ lib/locations/kilaueaVolcano1.ts
@@ -1,6 +1,6 @@
 import { LocationT } from "./index";
 
-const kona: LocationT = {
+const kilaueaVolcano: LocationT = {
   location: "kilauea1",
   name: "Kilauea Volcano",
   coordinates: [19.423892524549505, -155.28461347308874],
@@ -8,11 +8,20 @@ const kona: LocationT = {
   activities: ["Volcano"],
   date: new Date(2021, 1, 9),
   mainImage: "IMG_0612.jpeg",
-  imageInfo: [["kilaueavolcano1/cjiphone12", "Carlton's iPhone 12 Pro Max"]],
+  imageInfo: [
+    ["kilaueavolcano1/cjiphone12", "Carlton's iPhone 12 Pro Max"],
+    ["kilaueavolcano2/cjiphone12", "Carlton's iPhone 12 Pro Max"],
+  ],
   images: [
     [0, "IMG_0642.jpeg", 0, "", ""],
     [0, "IMG_0656.jpeg", 0, "", ""],
+    [1, "IMG_0816.jpeg", 0, "", ""],
+    [1, "IMG_0821.jpeg", 0, "", ""],
+    [1, "IMG_0826.jpeg", 0, "", ""],
+    [1, "IMG_0820.jpeg", 0, "", ""],
+    [1, "IMG_0824.jpeg", 0, "", ""],
+    [1, "IMG_0839.jpeg", 0, "", ""],
   ],
 };
 
-export default kona;
+export default kilaueaVolcano;
